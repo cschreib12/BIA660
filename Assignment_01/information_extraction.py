df5e5c33fa9ebccc028b1a1854e5f7e590589df2
cschreib12 <cschreib@stevens.edu>
2018-02-23 23:49:37 -0500

    commit

--- Assignment_01/information_extraction.py
+++ Assignment_01/information_extraction.py
@@ -1,12 +1,12 @@
 from __future__ import unicode_literals, print_function
 
 import re
-# import spacy
+#import spacy
 
 from pyclausie import ClausIE
 
 
-# nlp = spacy.load('en')
+#nlp = spacy.load('en')
 import en_core_web_sm
 nlp = en_core_web_sm.load()
 cl = ClausIE.get_instance()
@@ -54,9 +54,9 @@ pets = []
 trips = []
 
 
-def get_data_from_file(file_path='./chatbot_data.txt'):
+def get_data_from_file(file_path='./assignment_01.data'):
     with open(file_path) as infile:
-        cleaned_lines = [line.strip() for line in infile if not (line.startswith(('$$$', '###', '===')) or line == "\n")]
+        cleaned_lines = [line.strip() for line in infile if not (line.startswith(('$$$', '###', '===')) and line == "\n")]
 
     return cleaned_lines
 
@@ -107,6 +107,19 @@ def get_persons_pet(person_name):
             return thing
 
 
+def select_trip(departs_on=None, departs_to=None):
+    for trip in trips:
+        if trip.departs_on == departs_on and trip.departs_to == departs_to:
+            return trip
+
+
+def add_trip(location=None, time=None):
+    trip = select_trip(location, time)
+    if trip is None:
+        trip = Trip(location, time)
+        trips.append(trip)
+    return trip
+
 
 def process_relation_triplet(triplet):
     """
@@ -137,6 +150,7 @@ def process_relation_triplet(triplet):
 
     # also, if only one sentence
     # root = doc[:].root
+
     subj_span = doc.char_span(sentence.find(triplet.subject, len(triplet.subject)))
     obj_span = doc.char_span(sentence.find(triplet.object), len(sentence))
 
@@ -153,7 +167,7 @@ def process_relation_triplet(triplet):
 
     # Process (PERSON, likes, PERSON) relations
     if root.lemma_ == 'like':
-        if triplet.subject in [e.text for e in doc.ents if e.label_ == 'PERSON'] and triplet.object in [e.text for e in doc.ents if e.label_ == 'PERSON']:
+        if triplet.subject in [e.text for e in doc.ents if e.label_ == 'PERSON' or 'ORG'] and triplet.object in [e.text for e in doc.ents if e.label_ == 'PERSON']:
             s = add_person(triplet.subject)
             o = add_person(triplet.object)
             s.likes.append(o)
@@ -191,6 +205,20 @@ def process_relation_triplet(triplet):
 
             s_person.has.append(pet)
 
+    # Process (PERSON, has, PET)
+    if root.lemma_ == 'have' and ('dog' in triplet.object or 'cat' in triplet.object):
+        pet_type = 'dog' if 'dog' in triplet.object else 'cat'
+        person = add_person(triplet.subject)
+
+        person.addPet(pet_type)
+
+    # Process (PERSON, travels, TRIP)
+    if root.lemma_ in ['take', 'fly', 'leave', 'go'] and 'GPE' in [e.label_ for e in doc.ents]:
+        person = add_person(triplet.subject)
+        depart_to = [e.text for e in doc.ents if e.label_ == 'GPE'][0]
+        departs_on = [e.text for e in doc.ents if e.label_ == 'DATE'][0]
+        person.addTrip(departs_on=departs_on, departs_to=depart_to)
+
 
 def preprocess_question(question):
     # remove articles: a, an, the
@@ -209,15 +237,12 @@ def preprocess_question(question):
 
 def has_question_word(string):
     # note: there are other question words
-    for qword in ('who', 'what'):
+    for qword in ('who', 'what', 'when', 'where', 'does'):
         if qword in string.lower():
             return True
 
     return False
 
-
-def make_sentence_from_triplet(triplet):
-    return triplet.subject + ' ' + triplet.predicate + ' ' + triplet.object
 
 def answer_question(question=' '):
     while question[-1] != '?':
@@ -226,26 +251,29 @@ def answer_question(question=' '):
         if question[-1] != '?':
             print('This is not a question... please try again')
 
-    # my question preprocesser may break things. This was just a demo.
-    # Don't think that you have to use it for all questions, or even that you have to use it at all.
-    # Don't use my code blindly. Think critically about what it is doing and whether or not that's what you want.
+    cl = ClausIE.get_instance()
     q_trip = cl.extract_triples([preprocess_question(question)])[0]
 
-    triplet_sentence = make_sentence_from_triplet(q_trip) + '?'
-    doc = nlp(triplet_sentence)
+    triplet_sentence = q_trip.subject + ' ' + q_trip.predicate + ' ' + q_trip.object
+    doc = nlp(unicode(triplet_sentence))
     root = doc[:].root
 
     # retrieve answers for questions like (WHO, has, PET)
     # here's one just for dogs
     # you should really check the verb... this is just an example not the best way to do things
-    if q_trip.subject.lower() == 'who' and q_trip.object == 'dog':
+    if q_trip.subject.lower() == 'who' and root.lemma_ == 'have':
 
         answer = '{} has a {} named {}.'
-
-        for person in persons:
-            pet = get_persons_pet(person.name)
-            if pet and pet.type == 'dog':
-                print(answer.format(person.name, 'dog', pet.name))
+        if q_trip.object == 'dog':
+            for person in persons:
+                pet = get_persons_pet(person.name)
+                if pet and pet.type == 'dog':
+                    print(answer.format(person.name, pet.type, pet.name))
+        elif q_trip.object == 'cat':
+            for person in persons:
+                pet = get_persons_pet(person.name)
+                if pet and pet.type == 'cat':
+                    print(answer.format(person.name, pet.type, pet.name))
 
     # retrieve answers for questions like (WHO, like, PERSON)
     # again this is just an example, NOT the best way to do things. That's for you to figure out.
@@ -258,10 +286,20 @@ def answer_question(question=' '):
             if person.name != q_trip.object and liked_person in person.likes:
                 print(answer.format(person.name, liked_person.name))
 
+    elif q_trip.subject.lower() == 'does' and q_trip.predicate == 'like' and q_trip.object in [e.text for e in doc.ents if e.label_ == 'PERSON']:
+        person = select_trip(q_trip.subject)
+        liked_person = select_person(q_trip.object)
+        if liked_person in person.likes:
+            print(person.name, 'likes ', liked_person.name)
+        else:
+            print(person.name, 'does not like ', liked_person.name)
 
-def process_data_from_input_file(path='assignment_01.data'):
-    sents = get_data_from_file(path)
 
+
+
+def process_data_from_input_file():
+    sents = get_data_from_file()
+    cl = ClausIE.get_instance()
     triples = cl.extract_triples(sents)
 
     for t in triples:
@@ -269,10 +307,9 @@ def process_data_from_input_file(path='assignment_01.data'):
             process_relation_triplet(t)
         except:
             pass
-    return triples
 
 def main():
-    triples = process_data_from_input_file()
+    process_data_from_input_file()
     answer_question()
 
 
